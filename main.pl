888c4a13093fdf2bb29b18e94d6c25059fb7e0d2
Matheus Yamamoto Dias <matheusyamas@gmail.com>
2025-09-26 14:10:02 -0300

    v0.2 - implementado calculaPon

--- main.pl
+++ main.pl
@@ -2,28 +2,29 @@
 :- consult('base_conhecimento.pl').
 
 %Predicado que será enviado para o aluno 3
-recomenda(ListaFinalOrdenada) :-
+recomenda(ListaOrdenada, Justificativa) :-
 
     write('Lógica de recomendação a ser implementada'), nl.
 
-calcula_pontuacao(Trilha, PontuacaoFinal) :-
+calcula_pontuacao(Trilha, Pontuacao, Justificativa) :-
 
     write('Logica de calculo de pontuacao a ser implementada para a trilha: '), write(Trilha), nl.
 
-soma_pontos_perfis([], 0).
+soma_pontos_perfis([], 0, []).
 
-soma_pontos_perfis([PrimeiroPerfil | RestoDosPerfis], SomaTotal) :-
-
-    pontos_por_perfil(PrimeiroPerfil, PontosGuardados),
-    soma_pontos_perfis( RestoDosPerfis, PontosRestante),
+soma_pontos_perfis([PrimeiroPerfil | RestoDosPerfis], SomaTotal, JustificativaFormada) :-
+    pontos_por_perfil(PrimeiroPerfil, PontosGuardados, JustificativaGuardada),
+    soma_pontos_perfis( RestoDosPerfis, PontosRestante, JustificativaRestante),
     %debug: write('Sucesso, recursao deu certo'), nl,
-    SomaTotal is PontosGuardados + PontosRestante.
+    SomaTotal is PontosGuardados + PontosRestante,
+    append(JustificativaGuardada, JustificativaRestante, JustificativaFormada).
 
-pontos_por_perfil(perfil(Trilha, Caracteristica, Peso), Pontos) :-
+pontos_por_perfil(perfil(Trilha, Caracteristica, Peso), Pontos, [Justificativa]) :-
     % debug: write('if rodou: '), write(Caracteristica), nl,
     pergunta(ID, _, Caracteristica),
     resposta(ID, s), !,
-    Pontos is Peso.
+    Pontos is Peso,
+    swritef(Justificativa, 'Afinidade com %w (%w pts)', [Caracteristica, Peso]).
     %debug: write('Sucesso, if concluido'), nl.
 
-pontos_por_perfil(_, 0)
+pontos_por_perfil(_, 0, [])
