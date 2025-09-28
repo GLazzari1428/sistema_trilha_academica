d8dddb5eb970dce9402b3808ad3e5312f2e523e2
Matheus Yamamoto Dias <matheusyamas@gmail.com>
2025-09-28 15:21:23 -0300

    v0.4.1 - Lógica principal finalizada

--- main.pl
+++ main.pl
@@ -3,7 +3,7 @@
 
 %Predicado que será enviado para o aluno 3, e o predicado chefe.
 recomenda(ListaOrdenadaFinal, ListaJustificativaFinal) :-
-    %Procura as trilhas, pontuação e justificativa, para montar uma lista
+    %Procura as trilhas, pontuação e justificativa, para montar uma lista não ordenada com esses dados
     findall(
         [Trilha, Pontuacao, Justificativa],
         (trilha(Trilha, _), calcula_pontuacao(Trilha, Pontuacao, Justificativa)),
@@ -29,22 +29,19 @@ soma_pontos_perfis([], 0, []).
 soma_pontos_perfis([PrimeiroPerfil | RestoDosPerfis], SomaTotal, JustificativaFormada) :-
     pontos_por_perfil(PrimeiroPerfil, PontosGuardados, JustificativaGuardada),
     soma_pontos_perfis( RestoDosPerfis, PontosRestante, JustificativaRestante),
-    %debug: write('Sucesso, recursao deu certo'), nl,
     SomaTotal is PontosGuardados + PontosRestante,
     append(JustificativaGuardada, JustificativaRestante, JustificativaFormada).
 
 %Calcula o resultado de um único perfil a partir das respostas de s/n
 pontos_por_perfil(perfil(Trilha, Caracteristica, Peso), Pontos, [Justificativa]) :-
-    % debug: write('if rodou: '), write(Caracteristica), nl,
     pergunta(ID, _, Caracteristica),
     resposta(ID, s), !,
     Pontos is Peso,
     swritef(Justificativa, 'Afinidade com %w (%w pts)', [Caracteristica, Peso]).
-    %debug: write('Sucesso, if concluido'), nl.
 
 pontos_por_perfil(_, 0, []).
 
-%Os dois predicados abaixo são para completar o recomenda, fazendo a ordenação e separação das listas respectivamente
+%Os dois predicados abaixo são para completar o recomenda, fazendo a ordenação e a separação das listas respectivamente
 comparar_pontuacoes(Valor, [_Trilha1, Pontuacao1, _], [_Trilha2, Pontuacao2, _]) :-
     ( Pontuacao1 > Pontuacao2 -> Valor = '<'; 
     Pontuacao1 < Pontuacao2 -> Valor = '>';        
